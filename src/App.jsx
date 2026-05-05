0e680356b94fc163824df8d7bb674a58fc7151f5
rohitarora <rohitarora15820@gmail.com>
2026-05-05 16:53:53 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,14 @@
 import Header from "./components/Header";
-import HomePage from "./pages/HomePage";
+import HomePage from "./Pages/HomePage";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      {/* Header fixed hai — har page par dikhega */}
       <Header />
 
       <Routes>
         <Route path="/" element={<HomePage />} />
-        {/* <Route path="/services"  element={<ServicesPage />} /> */}
-        {/* <Route path="/about"     element={<AboutPage />} /> */}
-        {/* <Route path="/portfolio" element={<PortfolioPage />} /> */}
-        {/* <Route path="/contact"   element={<ContactPage />} /> */}
       </Routes>
     </>
   );
